2c5a608557496e9f8f565982193238d3fef26bb8
Lena Machado <lenamachado73@gmail.com>
2023-06-01 17:26:22 +0100

    guess

---
+++ src/main/java/com/mycompany/finalprojectgame/FinalProjectGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.finalprojectgame;
+
+/**
+ *
+ * @author TESTER
+ */
+public class FinalProjectGame {
+
+    public static void main(String[] args) {
+      new GuessNumber().setVisible(true);
+    }
+}
